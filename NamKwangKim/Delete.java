f1bed70ae8b3023377a38db1ad2c0ea53f38bd85
KimNamKwang <durante0916@gmail.com>
2022-10-25 15:21:21 +0900

    커밋

---
+++ NamKwangKim/Delete.java
@@ -0,0 +1,17 @@
+import java.text.Normalizer.Form;
+import java.util.Scanner;
+
+public class Delete{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+
+
+
+
+        
+        // if(input == user.USER_ID && input == user. ){
+
+        // }
+    }
+}
