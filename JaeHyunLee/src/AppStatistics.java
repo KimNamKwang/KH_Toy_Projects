4a0e27baea88473dc9a0ec29a3a05bead68b8ce8
dwogus6893 <dwogus6893@naver.com>
2022-10-26 14:11:02 +0900

    2022-10-26

--- JaeHyunLee/src/AppStatistics.java
+++ JaeHyunLee/src/AppStatistics.java
@@ -1,31 +1,45 @@
 import java.sql.*;
 import java.util.Scanner;
-
+//Statement == sql쿼리문을 보내기위한 객체
 public class AppStatistics {
-    public void StatisticsFunction(Statement statement, Scanner scanner) {
-        String input = scanner.next();
-        System.out.print("- 이름을 입력하세요 : ");
-        String checkName = scanner.nextLine();
-        System.out.print("- 비밀번호를 입력하세요 : ");
-        String checkPhoneNumber = scanner.nextLine();
+    public void StatisticsFunction(Statement statement) {
+        
+        System.out.println("-------------------------------------------------------------------------------");
+        System.out.println("");
+        System.out.println("통계");
+        
+        String query =("SELECT QUESTION_ID,
+        SUM(IF(ANSWER_ID = "ANSWER1",1,0) ) AS 'ANSWER1',
+        SUM(IF(ANSWER_ID = "ANSWER2",1,0) ) AS 'ANSWER2',
+        SUM(IF(ANSWER_ID = "ANSWER3",1,0) ) AS 'ANSWER3',
+        SUM(IF(ANSWER_ID = "ANSWER4",1,0) ) AS 'ANSWER4',
+        SUM(IF(ANSWER_ID = "ANSWER5",1,0) ) AS 'ANSWER5'
+        FROM survey
+        GROUP BY QUESTION_ID
+        WITH ROLLUP"
+        
+        
+        
+        
+        
+        );
 
-        String query = "SELECT NAME,PHONE_NUMBER FROM user WHERE NAME = "+ "\"" + checkName + "\""  +" AND PHONE_NUMBER = " + "\""+ checkPhoneNumber + "\"" + ";";
-        ResultSet resultSet;
+
+
+        ResultSet resultSet = null;
+        //String query = "SELECT * FROM user;";
+               
         try {
-            resultSet = statement.executeQuery(query);
 
-            if(resultSet.next() == false){
-                System.out.println("-------------------------------");
-                System.out.println("회원정보가 없습니다.");
-                System.out.println("-------------------------------");
+            resultSet = statement.executeQuery(query);
+                            
+            while(resultSet.next()){
+                System.out.println(resultSet.getString(1)+"\t"+resultSet.getString(2)+"\t"+resultSet.getString(3)+"\t"+resultSet.getString(4)+"\t"+resultSet.getString(5)+"\t"+resultSet.getString(6)+"\t");
             }
-            else{
-                System.out.println("-------------------------------");
-                System.out.println("이미 가입된 회원입니다.");
-                System.out.println("-------------------------------");
-            } 
+
             
         } catch (SQLException e) {
+           System.out.println("조회 실패!");
             e.printStackTrace();
         }
     }
