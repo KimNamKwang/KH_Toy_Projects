29614fbed4fb5ffc8ea9d333f6fba2529263e7cf
dwogus6893 <dwogus6893@naver.com>
2022-10-26 12:40:30 +0900

    2022-10-26

--- JaeHyunLee/src/AppStatistics.java
+++ JaeHyunLee/src/AppStatistics.java
@@ -5,34 +5,30 @@ import java.sql.Statement;
 import java.util.Scanner;
 
 public class AppStatistics {
-    public void StatisticsFunction(Statement statement){
+    public void StatisticsFunction(Statement statement) {
         Scanner scanner = new Scanner(System.in);
         String input = scanner.next();
-        System.out.println("----------------------------------------------------------------------------------------------\n");
-        System.out.println("설문내역을 확인 하실 수 있습니다. 이름과 비밀번호를 입력해주세요.");
-        
+
         System.out.print("이름을 입력해주세요 :" + input);
         String name = input;
         System.out.print("비밀번호를 입력해주세요 :" + input);
         String password = input;
-        String query = "SELECT * FROM user WHERE NAME = '"+name+"' AND PASSWORD = '"+password+"';";
+
+        String query = "SELECT * FROM user WHERE NAME = '" + name + "' AND PASSWORD = '" + password + "';";
+
         try {
-        
-        ResultSet resultSet = statement.executeQuery(query);
-        //ResultSet resultSet2 = statement.executeQuery(query);
-
-        if(resultSet.isBeforeFirst()) {
-
-            System.out.println("\n== 로그인에 성공했습니다. ==\n");
-            query = "DELETE FROM survey WHERE USER_ID IN (SELECT USER_ID FROM user WHERE NAME = " + input + "AND password =" + input + ");" ;
-            resultSet = statement.executeQuery(query);
+            ResultSet resultSet = statement.executeQuery(query);
+            if (resultSet.isBeforeFirst()) {
+                System.out.println("통계");
                 
-            statement.executeQuery(query);
+               
+
+            } else {
+                System.out.println("회원정보가 일치하지 않습니다, 다시 시도해주세요.");
             }
-    
-        }catch (Exception exception) {
+
+        } catch (Exception exception) {
             exception.printStackTrace();
-              }        
+        }
     }
-}
 }
